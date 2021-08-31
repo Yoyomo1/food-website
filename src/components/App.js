1ef52bc5da2d3be8a0ab57f748065c5e901f5e32
Marcus <marcusgchan35@gmail.com>
2021-08-31 12:31:02 -0700

    added router

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
 import "../styles/App.css";
 import ResultsPage from "./ResultsPage";
+import HomePage from "./HomePage";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 const App = () => {
   return (
     <Router>
       <Switch>
-        <Route path="/" exact></Route>
+        <Route path="/" exact>
+          <HomePage />
+        </Route>
         <Route path="/results">
           <ResultsPage />
         </Route>
