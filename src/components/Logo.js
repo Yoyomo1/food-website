691b0e5559335bc96de4634b89b1bf4a770f145e
John Doe <kentarolim10@gmail.com>
2021-09-05 11:35:52 -0700

    adds Logo link and styling

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import "../styles/Logo.css"
+import { Link } from "react-router-dom";
 
 const Logo = () => {
 
   const text = "F o o (d)"
 
   return (
-    <div className="border">
-      <div className="text">{text}</div>
-    </div>
+    <Link to='/' className='logo-link'>
+      <div className="border">
+        <div className="text">{text}</div>
+      </div>
+    </Link>
   )
 }
 
